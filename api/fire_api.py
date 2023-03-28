25fcdc0678d1a0836c448cc47f9a1b1c227f3654
Miewliie <miewliie@gmail.com>
2023-03-27 18:23:38 -0700

    [api] output latest fire data only to fire.json

--- api/fire_api.py
+++ api/fire_api.py
@@ -1,4 +1,5 @@
 import requests
+import os
 import json
 
 URL = "https://incidents.fire.ca.gov/umbraco/api/IncidentApi/List?"
@@ -12,13 +13,61 @@ def request_fire():
     return json_data
 
 
-def write_response(data):
-    with open('../outputs/fire.json', 'w', encoding='utf-8') as output_file:
+def write_json(data, file_path):
+    with open(file_path, 'w', encoding='utf-8') as output_file:
         json.dump(data, output_file)
         print("Done write response into json file")
 
 
+def read_json(file_path):
+    with open(file_path, 'r', encoding='utf-8') as output_file:
+        size = os.path.getsize(file_path)
+        if size > 0:
+            data = json.loads(output_file.read())
+        else:
+            data = None
+        return data
+
+
+def get_dup_fire_data(new_fire_data, prev):
+    prev_data = read_json(prev)
+    new_data = new_fire_data
+
+    dup_index_list = []
+    if prev_data:
+        for i in range(len(new_data)):
+            item = new_data[i]
+            for j in prev_data:
+                if item['UniqueId'] == j['UniqueId']:
+                    dup_index_list.insert(0, i)
+                    break
+        return dup_index_list
+    else:
+        return dup_index_list
+
+
+def save_latest_fire_data(new_json_data, prev):
+    write_json(new_json_data, prev)
+
+
+def prepare_ready_to_use_data(dup_index_list, new_fire_data, fire_json_output):
+    new_data = new_fire_data
+
+    if not dup_index_list == []:
+        for i in range(len(dup_index_list)):
+            del new_data[dup_index_list[i]]
+        write_json(new_data, fire_json_output)
+    else:
+        write_json(new_data, fire_json_output)
+
+
 if __name__ == '__main__':
+    prev_json_path = '../outputs/previous_fire_data.json'
+    fire_json_path = '../outputs/fire.json'
+
     fire_data = request_fire()
-    write_response(fire_data)
-    # TODO: compare new fire json data with the old one and save new data only
+    if fire_data:
+        dup_list = get_dup_fire_data(fire_data, prev_json_path)
+        save_latest_fire_data(fire_data, prev_json_path)
+        prepare_ready_to_use_data(dup_list, fire_data, fire_json_path)
+
