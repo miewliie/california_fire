53731dc85f94dbb7710eb32fa936c3b3a114188f
Miewliie <miewliie@gmail.com>
2023-04-27 13:35:19 -0700

    [output] fixed outputs path

--- start.py
+++ start.py
@@ -1,8 +1,8 @@
 from api.fire_api import *
 
 if __name__ == '__main__':
-    prev_json_path = '../outputs/previous_fire_data.json'
-    fire_json_path = '../outputs/fire.json'
+    prev_json_path = './outputs/previous_fire_data.json'
+    fire_json_path = './outputs/fire.json'
 
     fire_data = request_fire()
     if fire_data:
