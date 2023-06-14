e72a8b2d5a2512ad8650d34ad277f5af66d018af
Miewliie <miewliie@gmail.com>
2023-06-14 13:06:54 -0700

    [obsolete] remove obsolete function

--- california_fire/core/fire_api.py
+++ california_fire/core/fire_api.py
@@ -1,6 +1,5 @@
 import requests
 import datetime
-from utilities.json_handler import *
 
 URL = "https://incidents.fire.ca.gov/umbraco/api/IncidentApi/List?"
 STATUS = 'false'
@@ -13,42 +12,5 @@ def fetch_fires():
     json_data = response.json()
     return json_data
 
-
-def get_dup_fire_data(new_fire_data, prev):
-    """ This function will return the index of the duplicate data.
-    By comparing the UniqueId of the previous and latest response. """
-
-    prev_data = read_json(prev)
-    new_data = new_fire_data
-
-    dup_index_list = []
-    if prev_data:
-        for i in range(len(new_data)):
-            item = new_data[i]
-            for j in prev_data:
-                if item['UniqueId'] == j['UniqueId']:
-                    dup_index_list.insert(0, i)
-                    break
-        return dup_index_list
-    else:
-        return dup_index_list
-
-
-def save_latest_fire_data(new_json_data, prev):
-    """ Save the all latest response into previous json file. """
-    write_json(new_json_data, prev)
-
-
-def prepare_ready_to_use_data(dup_index_list, new_fire_data, fire_json_output):
-    """ Use duplication index list to remove the duplicate data out from latest response. """
-
-    new_data = new_fire_data
-
-    if not dup_index_list == []:
-        for i in range(len(dup_index_list)):
-            del new_data[dup_index_list[i]]
-        write_json(new_data, fire_json_output)
-    else:
-        write_json(new_data, fire_json_output)
 
 
