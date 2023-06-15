30d845facd297f802593da20ef4779639dd261f2
Miewliie <miewliie@gmail.com>
2023-06-15 11:29:46 -0700

    [tests] add test for draw fire

--- tests/draw/test_draw_fire.py
+++ tests/draw/test_draw_fire.py
@@ -23,18 +23,76 @@ class TestDrawFire(unittest.TestCase):
         y_pixel = convert_lat_to_y_pixel(image_height=im_height, latitude=latitude)
         self.assertEqual(expected_result, y_pixel)
 
-    def test_draw_fire_map(self):
+    def test_draw_fire_with_negative_acre(self):
         output_path: str = "tests/draw/test_data/fire_map.png"
         base_image_path: str = "tests/draw/test_data/california_base_map.png"
         fires: list[Fire] = [Fire(location='East Blue Ridge and Paiute, south of Wrightwood',
                                   date_time='2023-04-26T10:16:00Z',
-                                  acres_burned=150,
+                                  acres_burned=None,
                                   latitude=34.293103,
                                   longitude=-117.565833,
                                   url='https://incidents.fire.ca.gov/incidents/2023/4/26/nob-fire/')]
-        x: float = 756.3840142775193
-        y: float = 1535.8636294001003
+        with mock.patch('california_fire.draw.draw_fire.Image.open') as mock_open:
+            mock_open.Image.convert.return_value = mock.MagicMock(spec=Image, size=(1672, 1280), mode='RGBA')
+            with mock.patch('california_fire.draw.draw_fire.ImageDraw.Draw') as mock_draw:
+                mock_draw.ellipse.return_value = mock.MagicMock(spec=ImageDraw)
+                actual_result = draw_fire_map(base_image_path=base_image_path,
+                                              output_path=output_path, fire_data=fires)
 
+                mock_open.assert_called_once_with(base_image_path)
+                mock_draw.assert_called_once()
+                self.assertEqual(actual_result, output_path)
+
+    def test_draw_fire_map_with_one_to_one_thousand_acres(self):
+        output_path: str = "tests/draw/test_data/fire_map.png"
+        base_image_path: str = "tests/draw/test_data/california_base_map.png"
+        fires: list[Fire] = [Fire(location='East Blue Ridge and Paiute, south of Wrightwood',
+                                  date_time='2023-04-26T10:16:00Z',
+                                  acres_burned=1,
+                                  latitude=34.293103,
+                                  longitude=-117.565833,
+                                  url='https://incidents.fire.ca.gov/incidents/2023/4/26/nob-fire/')]
+
+        with mock.patch('california_fire.draw.draw_fire.Image.open') as mock_open:
+            mock_open.Image.convert.return_value = mock.MagicMock(spec=Image, size=(1672, 1280), mode='RGBA')
+            with mock.patch('california_fire.draw.draw_fire.ImageDraw.Draw') as mock_draw:
+                mock_draw.ellipse.return_value = mock.MagicMock(spec=ImageDraw)
+                actual_result = draw_fire_map(base_image_path=base_image_path,
+                                              output_path=output_path, fire_data=fires)
+
+                mock_open.assert_called_once_with(base_image_path)
+                mock_draw.assert_called_once()
+                self.assertEqual(actual_result, output_path)
+
+    def test_draw_fire_with_one_thousand_one_to_ten_thousand_acres(self):
+        output_path: str = "tests/draw/test_data/fire_map.png"
+        base_image_path: str = "tests/draw/test_data/california_base_map.png"
+        fires: list[Fire] = [Fire(location='East Blue Ridge and Paiute, south of Wrightwood',
+                                  date_time='2023-04-26T10:16:00Z',
+                                  acres_burned=1001,
+                                  latitude=34.293103,
+                                  longitude=-117.565833,
+                                  url='https://incidents.fire.ca.gov/incidents/2023/4/26/nob-fire/')]
+        with mock.patch('california_fire.draw.draw_fire.Image.open') as mock_open:
+            mock_open.Image.convert.return_value = mock.MagicMock(spec=Image, size=(1672, 1280), mode='RGBA')
+            with mock.patch('california_fire.draw.draw_fire.ImageDraw.Draw') as mock_draw:
+                mock_draw.ellipse.return_value = mock.MagicMock(spec=ImageDraw)
+                actual_result = draw_fire_map(base_image_path=base_image_path,
+                                              output_path=output_path, fire_data=fires)
+
+                mock_open.assert_called_once_with(base_image_path)
+                mock_draw.assert_called_once()
+                self.assertEqual(actual_result, output_path)
+
+    def test_draw_fire_with_more_than_ten_thousand_acres(self):
+        output_path: str = "tests/draw/test_data/fire_map.png"
+        base_image_path: str = "tests/draw/test_data/california_base_map.png"
+        fires: list[Fire] = [Fire(location='East Blue Ridge and Paiute, south of Wrightwood',
+                                  date_time='2023-04-26T10:16:00Z',
+                                  acres_burned=10001,
+                                  latitude=34.293103,
+                                  longitude=-117.565833,
+                                  url='https://incidents.fire.ca.gov/incidents/2023/4/26/nob-fire/')]
         with mock.patch('california_fire.draw.draw_fire.Image.open') as mock_open:
             mock_open.Image.convert.return_value = mock.MagicMock(spec=Image, size=(1672, 1280), mode='RGBA')
             with mock.patch('california_fire.draw.draw_fire.ImageDraw.Draw') as mock_draw:
